c6df7bdae41ca3def967d4471123551626f22471
Jaarabytes <xavierandole78@gmail.com>
2024-04-24 16:12:53 +0300

    Improve functionality
    Changed scope to global for buyer, seller and amount variables

--- script/Escrow.s.sol
+++ script/Escrow.s.sol
@@ -7,16 +7,15 @@ import "../src/Escrow.sol";
 
 contract CounterScript is Script {
     Escrow public escrow;
+    address public buyer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address public seller = 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720;
+    uint256 public amount = 1 ether;
     
     function setUp() public {
-        address buyer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        address seller = 0xa0Ee7A142d267C1f36714E4a8F75612F20a79720;
-        uint256 amount = 1 ether;
         escrow = new Escrow(buyer, seller, amount);
     }
 
     function run() public {
-        
         vm.broadcast();
     }
 }
