d2aac9c6c3473cc4cd8c2dca20eb2cdf961128d8
Jaarabytes <xavierandole78@gmail.com>
2024-04-24 17:50:15 +0300

    Finish-off tests. Update contract
    Add firmer tests using pytest. To rewrite tests in eth-brownie.

--- test/Escrow.t.py
+++ test/Escrow.t.py
@@ -20,4 +20,17 @@ contract = tester.deploy(bytecode, abi, args = [accounts[0], accounts[1], to_wei
 
 def test_deposit():
     #Send deposit from buyer
-    
+    contract.deposit(to_wei(5, "ether"), transact={"from": accounts[0]})
+
+    #Check buyer balance
+    assert contract.deposits(accounts[0]) == to_wei(5 , "ether")
+
+def test_withdraw():
+    #Bypass time requirement
+    tester.time_travel(block.timestamp + 21600)
+
+    #Seller withdraws funds
+    contract.withdraw(transact={"from": accounts[1]})
+
+    #Check seller's balance
+    assert test.get_balance(accounts[1]) == to_wei(9.5, "ether")
